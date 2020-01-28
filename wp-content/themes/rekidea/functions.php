58897798de1bddccfb14551aa6430908f5161ebd
swd762 <ilyakazalin@yandex.ru>
2020-01-28 08:51:49 +0400

    change style.css path

--- wp-content/themes/rekidea/functions.php
+++ wp-content/themes/rekidea/functions.php
@@ -3,7 +3,7 @@
 function add_styles_and_scripts () {
     //  ** Load styles **
   //  wp_enqueue_style('bootstrap-css', get_theme_file_uri('/bootstrap.min.css'));
-    wp_enqueue_style('style', get_theme_file_uri('/scss/style.css'));
+    wp_enqueue_style('style', get_theme_file_uri('/style.css'));
 
     // ** Load scripts **
 //    wp_enqueue_script('jquery', get_theme_file_uri('/js/jquery-1.12.0.min.js') );
