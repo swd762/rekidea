8e252d09afe691701a27fe84e7d1bb67e3d25dbb
swd762 <ilyakazalin@yandex.ru>
2020-02-05 14:33:00 +0400

    add laser cards block

--- wp-content/themes/rekidea/page-laser.php
+++ wp-content/themes/rekidea/page-laser.php
@@ -1,7 +1,7 @@
 <?php
 get_header();
 ?>
-<!--Breadcrumbs block-->
+<!--Breadcrumbs block *******************-->
 <div class="breadcrumbs">
     <div class="container">
         <p>Главная  > Лазерная резка</p>
@@ -9,7 +9,7 @@ get_header();
 </div>
 <!--*************************************-->
 
-<!--Laser top banner block-->
+<!--Laser top banner block ***************-->
 <div class="laser-top-banner">
 
 
@@ -19,10 +19,106 @@ get_header();
 </div>
 <!--**************************************-->
 
+<!--Laser cards block ********************-->
+<div class="laser-cards-block container">
+
+    <div class="card-laser">
+        <a href="#">
+            <img src="<?= get_template_directory_uri()?>/img/laser-cards/lazer-contur.png" alt="">
+            <h3>контурная фигурная лазерная резка</h3>
+            <p>Современный высокоскоростной лазерный станок поможет воплотить в жизнь самые смелые и сложные идеи.</p>
+            <a href="#" class="card-laser-button">
+                заказать
+            </a>
+        </a>
+    </div>
+    <div class="card-laser">
+        <a href="#">
+            <img src="<?= get_template_directory_uri()?>/img/laser-cards/flowers.png" alt="">
+            <h3>лазерная гравировка</h3>
+            <p>Нанесение рисунка, надписи, орнамента на поверхности оргстекла, дерева, кожи. Возможные варианты
+                - по контуру и с заполнением.
+            </p>
+            <a href="#" class="card-laser-button">
+                заказать
+            </a>
+        </a>
+    </div>
+    <div class="card-laser">
+        <a href="#">
+            <img src="<?= get_template_directory_uri()?>/img/laser-cards/flowers.png" alt="">
+            <h3>лазерная гравировка</h3>
+            <p>Нанесение рисунка, надписи, орнамента на поверхности оргстекла, дерева, кожи. Возможные варианты
+                - по контуру и с заполнением.
+            </p>
+            <a href="#" class="card-laser-button">
+                заказать
+            </a>
+        </a>
+    </div>
+    <div class="card-laser">
+        <a href="#">
+            <img src="<?= get_template_directory_uri()?>/img/laser-cards/flowers.png" alt="">
+            <h3>лазерная гравировка</h3>
+            <p>Нанесение рисунка, надписи, орнамента на поверхности оргстекла, дерева, кожи. Возможные варианты
+                - по контуру и с заполнением.
+            </p>
+            <a href="#" class="card-laser-button">
+                заказать
+            </a>
+        </a>
+    </div>
+    <div class="card-laser">
+        <a href="#">
+            <img src="<?= get_template_directory_uri()?>/img/laser-cards/lazer-contur.png" alt="">
+            <h3>контурная фигурная лазерная резка</h3>
+            <p>Современный высокоскоростной лазерный станок поможет воплотить в жизнь самые смелые и сложные идеи.</p>
+            <a href="#" class="card-laser-button">
+                заказать
+            </a>
+        </a>
+    </div>
+    <div class="card-laser">
+        <a href="#">
+            <img src="<?= get_template_directory_uri()?>/img/laser-cards/flowers.png" alt="">
+            <h3>лазерная гравировка</h3>
+            <p>Нанесение рисунка, надписи, орнамента на поверхности оргстекла, дерева, кожи. Возможные варианты
+                - по контуру и с заполнением.
+            </p>
+            <a href="#" class="card-laser-button">
+                заказать
+            </a>
+        </a>
+    </div>
+    <div class="card-laser">
+        <a href="#">
+            <img src="<?= get_template_directory_uri()?>/img/laser-cards/flowers.png" alt="">
+            <h3>лазерная гравировка</h3>
+            <p>Нанесение рисунка, надписи, орнамента на поверхности оргстекла, дерева, кожи. Возможные варианты
+                - по контуру и с заполнением.
+            </p>
+            <a href="#" class="card-laser-button">
+                заказать
+            </a>
+        </a>
+    </div>
+    <div class="card-laser">
+        <a href="#">
+            <img src="<?= get_template_directory_uri()?>/img/laser-cards/flowers.png" alt="">
+            <h3>лазерная гравировка</h3>
+            <p>Нанесение рисунка, надписи, орнамента на поверхности оргстекла, дерева, кожи. Возможные варианты
+                - по контуру и с заполнением.
+            </p>
+            <a href="#" class="card-laser-button">
+                заказать
+            </a>
+        </a>
+    </div>
 
 
 
-
+</div>
+<!--**************************************-->
 
 
 
