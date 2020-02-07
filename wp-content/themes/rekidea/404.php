4f75c8d74ce784e3b4b56e74a1784ee8173bf875
swd762 <ilyakazalin@yandex.ru>
2020-02-07 08:24:42 +0400

    correct 404 page, except mobile

--- wp-content/themes/rekidea/404.php
+++ wp-content/themes/rekidea/404.php
@@ -1,8 +1,27 @@
 <?php
 get_header();
 ?>
-
-
-
+<main>
+<div class="recunstruction-wrapper container clearfix">
+        <div class="reconstruction__content">
+            <h1>САЙТ НА РЕКОНСТРУКЦИИ!</h1>
+            <h3>Эта страница находится на реконструкции
+                и совсем скоро станет доступной для просмотра.
+            </h3>
+            <p>Приносим извинения за временные неудобства.</p>
+        </div>
+</div>
+<div class="questions-contact__wrapper container">
+    <div class="questions-contact__content">
+        <h2>у вас есть вопросы?</h2>
+        <p>Мы сэкономим ваше время! Звоните для консультаций
+            или оставьте заявку на обратный звонок.
+        </p>
+        <span>+ 7 (495) 369-20-79</span>
+        <a href="#" class="laser-btn-order">Заказать</a>
+    </div>
+    <img src="<?= get_template_directory_uri()?>/img/404/operator.png" alt="">
+</div>
+</main>
 <?php
 get_footer();
