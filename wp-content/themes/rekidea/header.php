d48d4e582bebf7376ff5215812e339890cf1ed5a
swd762 <ilyakazalin@yandex.ru>
2020-02-12 21:56:37 +0400

    carousels ready

--- wp-content/themes/rekidea/header.php
+++ wp-content/themes/rekidea/header.php
@@ -13,6 +13,7 @@ wp_head();
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11" />
+    <link href="<?= get_template_directory_uri()?>/img/rekidea-favicon.svg" rel="icon" type="image/svg+xml" />
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
