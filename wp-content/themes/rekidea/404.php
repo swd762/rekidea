220fd1d7b5c2b2d59362d0f4b834107c0a8e4d20
swd762 <ilyakazalin@yandex.ru>
2020-02-12 14:12:31 +0400

    update 404 page

--- wp-content/themes/rekidea/404.php
+++ wp-content/themes/rekidea/404.php
@@ -13,7 +13,7 @@ get_header();
 </div>
 <div class="recunstruction-mobile-wrapper">
     <div class="reconstruction-mobile__content">
-        <h1>САЙТ<br> НА РЕКОНСТРУКЦИИ!</h1>
+        <h1>САЙТ НА РЕКОНСТРУКЦИИ!</h1>
         <h3>Эта страница находится на реконструкции
             и совсем скоро станет доступной для просмотра.
         </h3>
@@ -21,7 +21,7 @@ get_header();
         <img src="<?= get_template_directory_uri()?>/img/404/reconstruction.jpg" />
     </div>
 </div>
-<div class="questions-contact__wrapper container">
+<div class="questions-contact__wrapper container err404">
     <div class="questions-contact__content">
         <h2>у вас есть вопросы?</h2>
         <p>Мы сэкономим ваше время! Звоните для консультаций
@@ -32,8 +32,7 @@ get_header();
             <a href="#" class="button">ПЕРЕЗВОНИТЬ МНЕ</a>
         </div>
     </div>
-    <img src="<?= get_template_directory_uri()?>/img/404/operator.png" alt="" class="question-operator">
-    <img src="<?= get_template_directory_uri()?>/img/404/operator.png" alt="" class="question-operator-mobile">
+    <img src="<?= get_template_directory_uri()?>/img/404/operator.png" alt="" class="question-operator er404">
 </div>
 </main>
 <?php
