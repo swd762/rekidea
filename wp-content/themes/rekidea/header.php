440c6fd984325972f1ce84d8fd939c7b1e177f84
kazalin <shadow762@list.ru>
2020-02-13 13:12:49 +0300

    changed mobile phone in header

--- wp-content/themes/rekidea/header.php
+++ wp-content/themes/rekidea/header.php
@@ -227,7 +227,7 @@ wp_head();
                     <img src="<?= get_template_directory_uri()?>/img/search_icon.png">
                  </div>
                  <div class="header-mobile__contacts">
-                     <a href="tel:+74953692079"><img src="<?= get_template_directory_uri()?>/img/phone_icon.png"></a>
+                     <a href="tel:+79265832351"><img src="<?= get_template_directory_uri()?>/img/phone_icon.png"></a>
                  </div>
              </div>
          </div>
