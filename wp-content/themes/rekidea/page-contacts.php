e883357c6b1e44349720a532e069081c37c7502e
swd762 <ilyakazalin@yandex.ru>
2020-03-24 23:11:01 +0400

    complete contacts page, NEW DB INCLUDED

--- wp-content/themes/rekidea/page-contacts.php
+++ wp-content/themes/rekidea/page-contacts.php
@@ -56,7 +56,7 @@ get_header();
 
 
     </div>
-    <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=<ваш API-ключ>" type="text/javascript"></script>
+    <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=71928d9b-4703-4998-834c-3cbb0db19b06" type="text/javascript"></script>
     <script type="text/javascript">
         // Функция ymaps.ready() будет вызвана, когда
         // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
@@ -64,8 +64,8 @@ get_header();
 
         function init() {
             var myMap = new ymaps.Map("map", {
-                center: [55.76, 37.64],
-                zoom: 12,
+                center: [55.784268, 37.706931],
+                zoom: 16,
                 controls: [
                     'zoomControl', // Ползунок масштаба
                     'rulerControl', // Линейка
@@ -73,89 +73,42 @@ get_header();
                     'trafficControl', // Пробки
                     'typeSelector', // Переключатель слоев карты
                     'fullscreenControl', // Полноэкранный режим
-                    // Поисковая строка
-                    // new ymaps.control.SearchControl({
-                    //     options: {
-                    //         // вид - поисковая строка
-                    //         size: 'large',
-                    //         // Включим возможность искать не только топонимы, но и организации.
-                    //         provider: 'yandex#search'
-                    //     }
-                    // })
                 ]
             });
-
-            ymaps.route([
-                'Москва, улица Крылатские холмы',
+            var myPlacemark = new ymaps.Placemark([55.787030, 37.705520],
                 {
-                    point: 'Москва, метро Молодежная',
-                    // метро "Молодежная" - транзитная точка
-                    // (проезжать через эту точку, но не останавливаться в ней).
-                    type: 'viaPoint'
+                    balloonContentBody:'<div style="display: flex;flex-direction: column;align-items: center">3-я проходная<b>Рекидея</b></div>'
                 },
-                [55.731272, 37.447198], // метро "Кунцевская".
-                'Москва, метро Пионерская'
-               ]).then(function (route) {
-                myMap.geoObjects.add(route);
-                // Зададим содержание иконок начальной и конечной точкам маршрута.
-                // С помощью метода getWayPoints() получаем массив точек маршрута.
-                // Массив транзитных точек маршрута можно получить с помощью метода getViaPoints.
-                // var points = route.getWayPoints(),
-                //     lastPoint = points.getLength() - 1;
-                // // Задаем стиль метки - иконки будут красного цвета, и
-                // // их изображения будут растягиваться под контент.
-                // points.options.set('preset', 'islands#redStretchyIcon');
-                // // Задаем контент меток в начальной и конечной точках.
-                // points.get(0).properties.set('iconContent', 'Точка отправления');
-                // points.get(lastPoint).properties.set('iconContent', 'Точка прибытия');
+                {
+                    iconLayout: 'default#image', // обозначаем что будет использоваться пользовательское изображение
+                    iconImageHref: '<?= get_template_directory_uri()?>/img/checkpoint-rekidea.svg',  // указываем путь к картинке которая будет служить меткой
+                    iconImageSize: [60, 100], // указываем размер изображения
+                    hideIconOnBalloonOpen: false,
+                    iconImageOffset: [-28, -70] // обозначаем сдвиг от левого верхнего угла к точке изображения метки .
+                });
 
-                // Проанализируем маршрут по сегментам.
-                // Сегмент - участок маршрута, который нужно проехать до следующего
-                // изменения направления движения.
-                // Для того, чтобы получить сегменты маршрута, сначала необходимо получить
-                // отдельно каждый путь маршрута.
-                // Весь маршрут делится на два пути:
-                // 1) от улицы Крылатские холмы до станции "Кунцевская";
-                // 2) от станции "Кунцевская" до "Пионерская".
-
-                // var moveList = 'Трогаемся,</br>',
-                //     way,
-                //     segments;
-                // // Получаем массив путей.
-                // for (var i = 0; i < route.getPaths().getLength(); i++) {
-                //     way = route.getPaths().get(i);
-                //     segments = way.getSegments();
-                //     for (var j = 0; j < segments.length; j++) {
-                //         var street = segments[j].getStreet();
-                //         moveList += ('Едем ' + segments[j].getHumanAction() + (street ? ' на ' + street : '') + ', проезжаем ' + segments[j].getLength() + ' м.,');
-                //         moveList += '</br>'
-                //     }
-                // }
-                // moveList += 'Останавливаемся.';
-                // // Выводим маршрутный лист.
-                // $('#list').append(moveList);
+            myMap.geoObjects.add(myPlacemark);
+            myPlacemark.balloon.open();
+            var multiRoute = new ymaps.multiRouter.MultiRoute({
+                referencePoints: [
+                    'Москва, м. Электрозаводская',
+                    [55.787030, 37.705520]
+                ],
+                params: {
+                    // Тип маршрута: на общественном транспорте.
+                    routingMode: "pedestrian"
+                }
+            }, {
+                // Автоматически устанавливать границы карты так,
+                // чтобы маршрут был виден целиком.
+                boundsAutoApply: true,
+                wayPointVisible:false,
+                routeActiveMarkerVisible: false
             });
 
-
-
-
-            // Добавление
-            //
-            //
-            //
-            // метки
-            // https://tech.yandex.ru/maps/doc/jsapi/2.1/ref/reference/Placemark-docpage/
-            // var myPlacemark = new ymaps.Placemark([55.76, 37.64], {
-            //     // Хинт показывается при наведении мышкой на иконку метки.
-            //     hintContent: 'Содержимое всплывающей подсказки',
-            //     // Балун откроется при клике по метке.
-            //     balloonContent: 'Содержимое балуна'
-            // });
-            //
-            // // После того как метка была создана, добавляем её на карту.
-            // myMap.geoObjects.add(myPlacemark);
+// Добавление маршрута на карту.
+            myMap.geoObjects.add(multiRoute);
             myMap.behaviors.disable('scrollZoom');
-
         }
     </script>
 
