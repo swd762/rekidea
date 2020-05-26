026e8938b23864bff23bdeb43c5ed1d340d39dfe
swd762 <ilyakazalin@yandex.ru>
2020-05-26 15:40:16 +0400

    corrects some errors

---
+++ wp-content/themes/rekidea/page-shop-mobile-stands.php
@@ -0,0 +1,12 @@
+<?php
+get_header();
+?>
+
+<!--Breadcrumbs block *******************-->
+<div class="breadcrumbs">
+    <div class="container">
+        <p><a href="/">Главная</a> > <a href="/price">Цены</a> > <a href="">Мобильные стенды</a> > Roll Up</p>
+    </div>
+</div>
+<!--*************************************-->
+
