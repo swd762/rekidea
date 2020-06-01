044edb6ab523616678dbec43b0a4f03bcffb56fe
swd762 <ilyakazalin@yandex.ru>
2020-06-01 22:59:29 +0400

    shop card -- good slider with navigation added

--- wp-content/themes/rekidea/partials/price-product.inc.php
+++ wp-content/themes/rekidea/partials/price-product.inc.php
@@ -11,6 +11,18 @@
                 <a href="#"><img src="<?= get_template_directory_uri() ?>/img/test/roll-3.jpg" alt="shop thumbnail"></a>
             </div>
         </div>
+        <div class="thumbs-slider">
+            <div class="slide">
+                <img src="<?= get_template_directory_uri() ?>/img/test/roll-2.jpg" alt="shop thumbnail">
+            </div>
+            <div class="slide">
+                <img src="<?= get_template_directory_uri() ?>/img/test/roll-1.jpg" alt="shop thumbnail">
+            </div>
+            <div class="slide">
+                <img src="<?= get_template_directory_uri() ?>/img/test/roll-3.jpg" alt="shop thumbnail">
+            </div>
+
+        </div>
     </div>
     <div class="shop-card__main">
         <div class="shop-card__main-header">
@@ -63,16 +75,31 @@
             slidesToScroll: 1,
             arrows: true,
             dots: false,
-            autoplay: true,
+            // autoplay: true,
             prevArrow: '<button class="arrow-prev"></button>',
             nextArrow: '<button class="arrow-next"></button>',
-            autoplaySpeed: 2000
+            // asNavFor: '.thumbs-slider',
+            // autoplaySpeed: 2000
             // centerMode: true,
-            // adaptiveHeight: true,
+             adaptiveHeight: false,
             // focusOnSelect: true
         });
+        $('.thumbs-slider').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: true,
+            dots: false,
+            autoplay: false,
+            prevArrow: '<button class="arrow-prev"></button>',
+            nextArrow: '<button class="arrow-next"></button>',
+            asNavFor: '.samples-slider',
+            // autoplaySpeed: 2000,
+             centerMode: true,
+            // adaptiveHeight: true,
+            focusOnSelect: true
+        });
     });
-
 
 </script>
 
