e774ce0225b1d7e4024ab3f78723dc8eabc6afb6
swd762 <ilyakazalin@yandex.ru>
2020-06-21 23:28:43 +0400

    changed hot and hit icons algoritms

--- wp-content/themes/rekidea/page-shop-mobile-stands.php
+++ wp-content/themes/rekidea/page-shop-mobile-stands.php
@@ -186,7 +186,31 @@ while ($loop->have_posts()): $loop->the_post();
                             }
                             ?>
                             <div class="shop-card__thumb-slider">
-                                <div class="owl-carousel owl-theme slider-main owl-main<?= '-' . $index ?> <?= $offer_icon ?>">
+                                <?php if ($offer_icon != '') {
+                                    echo '<span class="';
+                                    echo $offer_icon;
+                                    echo '" onclick="open_hot();"><img src="';
+                                    echo get_template_directory_uri();
+                                    echo '/img/' . $offer_icon . '.png"></span>';
+                                }
+                                ?>
+                                <script>
+
+                                        function open_hot() {
+                                            jQuery.fancybox.open({
+                                                src: '#hot-modal',
+                                                type: 'inline',
+                                                touch: false,
+                                                modal: false,
+                                                // closeBtn: true
+                                            });
+                                        }
+
+
+
+                                </script>
+
+                                <div class="owl-carousel owl-theme slider-main owl-main<?= '-' . $index ?> ">
                                     <?php foreach ($good['thumbs'] as $pics_index => $thumbs) { ?>
                                         <div class="item">
                                             <a class="fancybox" href="<?= isset($good['watermarks'][$pics_index]) ?
@@ -208,85 +232,7 @@ while ($loop->have_posts()): $loop->the_post();
                                     <?php } ?>
                                 </div>
                             </div>
-                            <script>
-                                jQuery(document).ready(function ($) {
 
-                                    var sync1 = $(".owl-main<?='-' . $index ?>");
-                                    var sync2 = $(".owl-thumbs<?='-' . $index ?>");
-                                    var slidesPerPage = 3; //globaly define number of elements per page
-                                    var syncedSecondary = true;
-
-                                    sync1.owlCarousel({
-                                        items: 1,
-                                        slideSpeed: 2000,
-                                        nav: true,
-                                        autoplay: false,
-                                        dots: true,
-                                        loop: true,
-                                        responsiveRefreshRate: 200,
-                                        navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
-                                    }).on('changed.owl.carousel', syncPosition);
-
-                                    sync2.on('initialized.owl.carousel', function () {
-                                        sync2.find(".owl-item").eq(0).addClass("current");
-                                    }).owlCarousel({
-                                        items: slidesPerPage,
-                                        dots: false,
-                                        nav: false,
-                                        smartSpeed: 200,
-                                        slideSpeed: 500,
-                                        // mouseDrag: false,
-                                        slideBy: 1, //alternatively you can slide by 1, this way the active slide
-                                        // will stick to the first item in the second carousel
-                                        responsiveRefreshRate: 100
-                                    }).on('changed.owl.carousel', syncPosition2);
-
-
-                                    function syncPosition(el) {
-                                        //if you set loop to false, you have to restore this next line
-                                        //var current = el.item.index;
-
-                                        //if you disable loop you have to comment this block
-                                        var count = el.item.count - 1;
-                                        var current = Math.round(el.item.index - (el.item.count / 2) - .5);
-
-                                        if (current < 0) {
-                                            current = count;
-                                        }
-                                        if (current > count) {
-                                            current = 0;
-                                        }
-
-                                        //end block
-
-                                        sync2.find(".owl-item").removeClass("current").eq(current).addClass("current");
-
-                                        var onscreen = sync2.find('.owl-item.active').length - 1;
-                                        var start = sync2.find('.owl-item.active').first().index();
-                                        var end = sync2.find('.owl-item.active').last().index();
-
-                                        if (current > end) {
-                                            sync2.data('owl.carousel').to(current, 100, true);
-                                        }
-                                        if (current < start) {
-                                            sync2.data('owl.carousel').to(current - onscreen, 100, true);
-                                        }
-                                    }
-
-                                    function syncPosition2(el) {
-                                        if (syncedSecondary) {
-                                            var number = el.item.index;
-                                            sync1.data('owl.carousel').to(number, 100, true);
-                                        }
-                                    }
-
-                                    sync2.on("click", ".owl-item", function (e) {
-                                        e.preventDefault();
-                                        var number = $(this).index();
-                                        sync1.data('owl.carousel').to(number, 300, true);
-                                    });
-                                })
-                            </script>
 
                             <div class="shop-card__main">
                                 <div class="shop-card__main-header">
@@ -380,6 +326,85 @@ while ($loop->have_posts()): $loop->the_post();
                                 </div>
                             </div>
                         </div>
+                        <script>
+                            jQuery(document).ready(function ($) {
+
+                                var sync1 = $(".owl-main<?='-' . $index ?>");
+                                var sync2 = $(".owl-thumbs<?='-' . $index ?>");
+                                var slidesPerPage = 3; //globaly define number of elements per page
+                                var syncedSecondary = true;
+
+                                sync1.owlCarousel({
+                                    items: 1,
+                                    slideSpeed: 2000,
+                                    nav: true,
+                                    autoplay: false,
+                                    dots: true,
+                                    loop: true,
+                                    responsiveRefreshRate: 200,
+                                    navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+                                }).on('changed.owl.carousel', syncPosition);
+
+                                sync2.on('initialized.owl.carousel', function () {
+                                    sync2.find(".owl-item").eq(0).addClass("current");
+                                }).owlCarousel({
+                                    items: slidesPerPage,
+                                    dots: false,
+                                    nav: false,
+                                    smartSpeed: 200,
+                                    slideSpeed: 500,
+                                    // mouseDrag: false,
+                                    slideBy: 1, //alternatively you can slide by 1, this way the active slide
+                                    // will stick to the first item in the second carousel
+                                    responsiveRefreshRate: 100
+                                }).on('changed.owl.carousel', syncPosition2);
+
+
+                                function syncPosition(el) {
+                                    //if you set loop to false, you have to restore this next line
+                                    //var current = el.item.index;
+
+                                    //if you disable loop you have to comment this block
+                                    var count = el.item.count - 1;
+                                    var current = Math.round(el.item.index - (el.item.count / 2) - .5);
+
+                                    if (current < 0) {
+                                        current = count;
+                                    }
+                                    if (current > count) {
+                                        current = 0;
+                                    }
+
+                                    //end block
+
+                                    sync2.find(".owl-item").removeClass("current").eq(current).addClass("current");
+
+                                    var onscreen = sync2.find('.owl-item.active').length - 1;
+                                    var start = sync2.find('.owl-item.active').first().index();
+                                    var end = sync2.find('.owl-item.active').last().index();
+
+                                    if (current > end) {
+                                        sync2.data('owl.carousel').to(current, 100, true);
+                                    }
+                                    if (current < start) {
+                                        sync2.data('owl.carousel').to(current - onscreen, 100, true);
+                                    }
+                                }
+
+                                function syncPosition2(el) {
+                                    if (syncedSecondary) {
+                                        var number = el.item.index;
+                                        sync1.data('owl.carousel').to(number, 100, true);
+                                    }
+                                }
+
+                                sync2.on("click", ".owl-item", function (e) {
+                                    e.preventDefault();
+                                    var number = $(this).index();
+                                    sync1.data('owl.carousel').to(number, 300, true);
+                                });
+                            })
+                        </script>
                         <?php
 //                        break;
                     } ?>
@@ -446,11 +471,20 @@ while ($loop->have_posts()): $loop->the_post();
         <!--****************************************************-->
     </main>
     <!--*** hot deal modal-->
-    <div class="hot-deal__modal">
+    <div class="hot-deal__modal" >
         <img src="<?= get_template_directory_uri() ?>/img/fire.svg" alt="hot deal">
         <h2>горячее предложение</h2>
         <h3>обратите внимание на цену!</h3>
         <div class="close-btn" onclick="closeHotDealModal()"><span></span><span></span></div>
+    </div>
+    <!--***-->
+
+    <!--*** hot modal-->
+    <div class="hot-modal" id="hot-modal">
+        <img src="<?= get_template_directory_uri() ?>/img/fire.svg" alt="hot deal">
+        <h2>горячее предложение</h2>
+        <h2>обратите внимание на цену!</h2>
+
     </div>
     <!--***-->
 
